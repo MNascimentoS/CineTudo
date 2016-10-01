16eb983d78ba7775dea4cea34b2c39c233b5eb76
Wellington Correia <wellington.correia02@gmail.com>
2016-10-01 20:48:05 -0300

    Adicionado uma consulta a tabela do banco de dados direcionada ao usuario para fazer login com o mesmo.

--- src/cinetudoproject/LoginFXMLController.java
+++ src/cinetudoproject/LoginFXMLController.java
@@ -10,6 +10,12 @@ import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import com.jfoenix.validation.RequiredFieldValidator;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.ParseException;
 import java.util.ResourceBundle;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -23,6 +29,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import javax.swing.JOptionPane;
+import model.Funcionario;
 
 /**
  *
@@ -64,7 +71,9 @@ public class LoginFXMLController implements Initializable {
     
     public void login(ActionEvent event) throws Exception
     {
-        if(tv_name.getText().equals("user") && tv_password.getText().equals("pass"))
+        Funcionario funcionario = buscaPorUser(tv_name.getText());
+        
+        if(tv_name.getText().equals(funcionario.getUser()) && tv_password.getText().equals(funcionario.getSenha()))
         {
               Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
               Parent root = FXMLLoader.load(getClass().getResource("MainFXML.fxml"));
@@ -78,4 +87,54 @@ public class LoginFXMLController implements Initializable {
         }
         
     }
+    
+     private Connection conexao() {
+            String url = "jdbc:mysql://localhost:3306/cinetudo";
+            String user = "root";
+            String password = "";
+
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                return DriverManager.getConnection(url, user, password);
+            } catch (ClassNotFoundException | SQLException e) {
+                e.printStackTrace();
+                if(e instanceof ClassNotFoundException) {
+                        System.err.println("VERIFIQUE SE O DRIVER DO BANCO DE DADOS ESTÁ NO CLASSPATH");
+                } else {
+                        System.err.println("VERIFIQUE SE O BANCO ESTÁ RODANDO E SE OS DADOS DE CONEXÃO ESTÃO CORRETOS");
+                }
+                System.exit(0);
+                // o sistema deverá sair antes de chegar aqui...
+                return null;
+            }
+	}
+     
+     public Funcionario buscaPorUser(String user) {
+		Funcionario func = null;
+                final String busca = "SELECT usuario, senha FROM funcionario WHERE usuario = ?";
+		try {
+			Connection conn = conexao();
+			PreparedStatement buscar = conn.prepareStatement(busca);
+			buscar.setString(1, user);
+			ResultSet resultadoBusca = buscar.executeQuery();
+			resultadoBusca.next();
+			func = buscaFuncionario(resultadoBusca);
+			buscar.close();
+			conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println("ERRO BUSCANDO CONTA COM USUARIO " + user);
+			System.exit(0);
+		} 
+		return func;
+	}
+     private Funcionario buscaFuncionario(ResultSet resultadoBusca) throws SQLException, ParseException {
+		Funcionario funcionario = new Funcionario();
+		funcionario.setUser(resultadoBusca.getString(1));
+		funcionario.setSenha(resultadoBusca.getString(2));
+                
+                System.err.println(funcionario.getNome());
+                System.err.println(funcionario.getSenha());
+		return funcionario;
+	}
 }
