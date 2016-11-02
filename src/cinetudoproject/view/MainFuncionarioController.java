2b5b72784102d4c9d6282e34ce434b9a16220a1f
José Diôgo <josejuniordsc@gmail.com>
2016-11-02 20:39:33 -0300

    adicionado botao link para busca de sessao

--- src/cinetudoproject/view/MainFuncionarioController.java
+++ src/cinetudoproject/view/MainFuncionarioController.java
@@ -4,11 +4,13 @@
  * and open the template in the editor.
  */
 package cinetudoproject.view;
+import cinetudoproject.model.dao.AssentoDAO;
 import cinetudoproject.model.dao.FilmeDAO;
 import cinetudoproject.model.dao.SessaoDAO;
 import cinetudoproject.model.dao.CinemaDAO;
 import cinetudoproject.model.dao.HorarioDAO;
 import cinetudoproject.model.dao.PromocaoDAO;
+import cinetudoproject.model.domain.Assento;
 import cinetudoproject.model.domain.Filme;
 import cinetudoproject.model.domain.Funcionario;
 import cinetudoproject.model.domain.Sessao;
